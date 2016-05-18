70ca630f5306ac65b9e1d2f7dee1a4973ed59f91
niney <niney@jiran.com>
2016-05-18 18:06:47 +0900

    5. service

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from './hero';
+// import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-hero-detail',
@@ -17,4 +18,6 @@ import { Hero } from './hero';
 export class HeroDetailComponent {
     @Input()
     hero: Hero;
+
+    // constructor(private heroService: HeroService) { }
 }
